49636f175e563e0583fd84930bd699c086d894c1
Gianni Henriques <gianni.henriques@epitech.eu>
2025-11-20 11:13:11 +0900

    fix(auth): use JWT_SECRET instead of SECRET_JWT

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { ConfigService } from "@nestjs/config";
     JwtModule.registerAsync({
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
-        secret: config.get<string>("SECRET_JWT"),
+        secret: config.get<string>("JWT_SECRET"),
         signOptions: { expiresIn: "1d" },
       }),
     }),
