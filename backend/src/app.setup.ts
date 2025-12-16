4524bc34fa22a0208b5e8b34072dac5817f52b27
Gianni Henriques <gianni.henriques@epitech.eu>
2025-12-16 17:58:11 +0900

    feat(backend/swagger): only initialize swagger in development mode

--- backend/src/app.setup.ts
+++ backend/src/app.setup.ts
@@ -5,12 +5,6 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 export function setupApp(app: INestApplication) {
   const configService = app.get(ConfigService);
   const apiPrefix = configService.get<string>("API_PREFIX") || "/api/v1";
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle("VigilArt API")
-    .setDescription("Official API documentation for the VigilArt application.")
-    .setVersion("0.0.1")
-    .build();
-  const documentFactory = SwaggerModule.createDocument(app, swaggerConfig);
 
   app.enableCors();
   app.setGlobalPrefix(apiPrefix);
@@ -21,6 +15,16 @@ export function setupApp(app: INestApplication) {
       transform: true,
     })
   );
-  SwaggerModule.setup("api/v1/documentation", app, documentFactory);
+
+  if (process.env.NODE_ENV !== "production") {
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle("VigilArt API")
+      .setDescription("Official API documentation for the VigilArt application.")
+      .setVersion("0.0.1")
+      .build();
+    const documentFactory = SwaggerModule.createDocument(app, swaggerConfig);
+
+    SwaggerModule.setup("api/v1/documentation", app, documentFactory);
+  }
   return app;
 }
