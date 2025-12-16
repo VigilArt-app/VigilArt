ab010ed5f3386033755ecb62d66102417d6bf2ef
Gianni Henriques <gianni.henriques@epitech.eu>
2025-12-16 17:55:58 +0900

    feat(backend): swagger setup for API documentation

--- backend/src/app.setup.ts
+++ backend/src/app.setup.ts
@@ -1,11 +1,18 @@
 import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 export function setupApp(app: INestApplication) {
   const configService = app.get(ConfigService);
   const apiPrefix = configService.get<string>("API_PREFIX") || "/api/v1";
-  app.enableCors();
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("VigilArt API")
+    .setDescription("Official API documentation for the VigilArt application.")
+    .setVersion("0.0.1")
+    .build();
+  const documentFactory = SwaggerModule.createDocument(app, swaggerConfig);
 
+  app.enableCors();
   app.setGlobalPrefix(apiPrefix);
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,5 +21,6 @@ export function setupApp(app: INestApplication) {
       transform: true,
     })
   );
+  SwaggerModule.setup("api/v1/documentation", app, documentFactory);
   return app;
 }
