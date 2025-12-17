e5451f3d1fddf159401258a10076bdb29d052775
Gianni Henriques <gianni.henriques@epitech.eu>
2025-12-18 01:14:13 +0900

    refactor(prisma): use process.env to avoid prisma throwing for invalid env (https://github.com/prisma/prisma/issues/28590#issuecomment-3557020911)

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -7,6 +7,6 @@ export default defineConfig({
     seed: "pnpm dlx tsx src/prisma/seeds/index.ts"
   },
   datasource: {
-    url: env("DATABASE_URL")
+    url: process.env.DATABASE_URL!
   }
 });
