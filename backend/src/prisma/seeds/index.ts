44b887617b44e617ab59f6b6bb07904021e49a9a
Sephorah ANIAMBOSSOU <sephorah.aniambossou@epitech.eu>
2025-12-25 18:05:00 -0500

    feat: add seeding

--- backend/src/prisma/seeds/index.ts
+++ backend/src/prisma/seeds/index.ts
@@ -1,11 +1,23 @@
 import { PrismaClient } from "@vigilart/shared/server";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { initialUsers, initialArtworks } from "./seed-db";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 async function main() {
   console.log("🌱 Starting seeding...");
+
+  for (const artist of initialUsers) {
+    await prisma.user.create({
+      data: artist,
+    });
+  }
+  for (const artwork of initialArtworks) {
+    await prisma.artwork.create({
+      data: artwork,
+    });
+  }
   console.log("✅ Seeding completed.");
 }
 
---
+++ backend/src/prisma/seeds/seed-db.ts
@@ -0,0 +1,65 @@
+import { SubscriptionTier } from "@vigilart/shared";
+
+export const initialArtworks = [
+  {
+    userId: "7b7a4390-087a-4598-a3a0-4e1562ff7082",
+    imageUri:
+      "https://i.pinimg.com/1200x/ea/e3/17/eae317bf285c3d834d72e5e386cd6ec3.jpg",
+    originalFilename: null,
+    contentType: null,
+    sizeBytes: null,
+    description: "Pearl OC",
+    lastScanAt: null,
+  },
+  {
+    userId: "7b7a4390-087a-4598-a3a0-4e1562ff7082",
+    imageUri:
+      "https://i.pinimg.com/1200x/91/6d/53/916d536e560e0c783e4134300ae0ac6f.jpg",
+    originalFilename: null,
+    contentType: null,
+    sizeBytes: null,
+    description: "Bloom",
+    lastScanAt: null,
+  },
+  {
+    userId: "7b7a4390-087a-4598-a3a0-4e1562ff7082",
+    imageUri:
+      "https://i.pinimg.com/1200x/91/6d/53/916d536e560e0c783e4134300ae0ac6f.jpg",
+    originalFilename: null,
+    contentType: null,
+    sizeBytes: null,
+    description: "Stella",
+    lastScanAt: null,
+  },
+  {
+    userId: "7b7a4390-087a-4598-a3a0-4e1562ff7082",
+    imageUri:
+      "https://i.pinimg.com/1200x/b1/bb/3c/b1bb3cb3cfaa6dc68a52bc28c46595d1.jpg",
+    originalFilename: null,
+    contentType: null,
+    sizeBytes: null,
+    description: "Layla",
+    lastScanAt: null,
+  },
+];
+
+export const initialUsers = [
+  {
+    id: "7b7a4390-087a-4598-a3a0-4e1562ff7082",
+    email: "gia_seeds@gmail.com",
+    password: "H4shed_password_",
+    firstName: "Gia",
+    lastName: "Seeds",
+    avatar: null,
+    subscriptionTier: SubscriptionTier.FREE,
+  },
+  {
+    id: "8a7a4390-087a-4598-a3a0-4e1562ff7082",
+    email: "amanda_rowles@gmail.com",
+    password: "H4shed_password_#",
+    firstName: "Amanda",
+    lastName: "Rowles",
+    avatar: null,
+    subscriptionTier: SubscriptionTier.FREE,
+  },
+];
