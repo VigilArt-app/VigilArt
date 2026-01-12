1036f4d8bc28fe2ce90bdec85464dceeeaf5eb33
Gianni Henriques <gianni.henriques@epitech.eu>
2026-01-12 19:46:42 +0900

    feat(shared/dmca-platform): add precise typing for formSchema property

--- shared/src/types/Dmca/Platform/index.ts
+++ shared/src/types/Dmca/Platform/index.ts
@@ -8,3 +8,11 @@ import {
 export type DmcaPlatformGet = z.infer<typeof DmcaPlatformGetSchema>;
 export type DmcaPlatformCreate = z.infer<typeof DmcaPlatformCreateSchema>;
 export type DmcaPlatformUpdate = z.infer<typeof DmcaPlatformUpdateSchema>;
+
+export type {
+    DmcaFieldType,
+    DmcaFormField,
+    DmcaFormGroup,
+    DmcaFormItem,
+    DmcaFormSchema
+} from "../../../schemas/Dmca/Platform";
