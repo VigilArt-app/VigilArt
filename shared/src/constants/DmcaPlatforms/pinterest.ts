4cf054448c36788264799b8372527cf1998d17ea
Gianni Henriques <gianni.henriques@epitech.eu>
2026-02-20 13:57:40 +0900

    fix(shared/constants): invalid pinterest urls

--- shared/src/constants/DmcaPlatforms/pinterest.ts
+++ shared/src/constants/DmcaPlatforms/pinterest.ts
@@ -3,9 +3,9 @@ import { DmcaPlatformCreate } from "../../types/Dmca/Platform";
 export const PinterestDmcaFormJSON = {
     slug: "PINTEREST",
     displayName: "Pinterest",
-    dmcaUrl: "https://en.pinterest.com/about/copyright/dmca-pin/",
+    dmcaUrl: "https://pinterest.com/about/copyright/dmca-pin/",
     email: "copyright@pinterest.com",
-    domain: "https://en.pinterest.com/",
+    domain: "https://pinterest.com/",
     websiteCategory: "ART_PLATFORMS",
     formSchema: []
 } as const satisfies DmcaPlatformCreate;
