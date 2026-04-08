c5704c61d0a149ab59c9358396a815167476d3f8
yasmine <yasmine.bedrane@epitech.eu>
2026-04-08 05:22:43 +0200

    mobile test: try to debug ci

--- mobile-app/lib/(api)/auth.dart
+++ mobile-app/lib/(api)/auth.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-
+//comment
 class ApiService {
   final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
   String serverUrl = "http://10.0.2.2:3001/api/v1";
