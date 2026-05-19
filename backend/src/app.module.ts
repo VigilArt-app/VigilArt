210dfefac1ee541489f553693ff063865cbed17c
Gianni Henriques <gianni.henriques@epitech.eu>
2026-05-19 19:13:20 +0900

    feat(backend/app.module): set global cache handler

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -12,6 +12,8 @@ import { UsersModule } from "./users/users.module";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { ScheduleModule } from "@nestjs/schedule";
 import { BullModule } from "@nestjs/bullmq";
+import { CacheModule } from "@nestjs/cache-manager";
+import { createKeyv } from "@keyv/redis";
 import { AuthModule } from "./auth/auth.module";
 import { VisionModule } from "./vision/vision.module";
 import { ArtworksModule } from "./artworks/artworks.module";
@@ -37,6 +39,15 @@ import { GoogleLensModule } from "./googlelens/googlelens.module";
         connection: {
           url: config.getOrThrow<string>("REDIS_URL")
         }
+      })
+    }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        stores: [createKeyv(config.getOrThrow<string>("REDIS_URL"))],
+        ttl: 1 * 60 * 60 * 1000
       })
     }),
     UsersModule,
